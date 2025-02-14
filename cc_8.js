bf9381aa02add8ce90505bc75ff1135358a8d97d
mallikanicole <malmichael23@gmail.com>
2025-02-14 01:47:36 -0500

    Task 1 - Employee Salary Calculation

--- cc_8.js
+++ cc_8.js
@@ -1,1 +1,6 @@
-//Task 1
+//Task 1 Function Declaration
+function calculateSalary(baseSalary, bonus, taxRate){
+    return netSalary = (baseSalary + bonus) - (baseSalary * taxRate)
+};//declare net salary function
+console.log(`Net Salary:$${calculateSalary(5000, 500, 0.1).toFixed(2)}`); // Expected output: "Net Salary: $5000.00"
+console.log(`Net Salary:$${calculateSalary(7000, 1000, 0.15).toFixed(2)}`); // Expected output: "Net Salary: $6950.00"
