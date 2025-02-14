ba1b3ca1875a5e96d1265853aae691d7b46cdbd8
mallikanicole <malmichael23@gmail.com>
2025-02-14 11:10:14 -0500

    Task 3 - Service Fee Calculation

--- cc_8.js
+++ cc_8.js
@@ -12,3 +12,16 @@ function calculateDiscount(price, discountRate){
 };//use price and discount rate to find total price
 console.log(`Final Price:$${calculateDiscount(100, 0.2).toFixed(2)}`); // Expected output: "Final Price: $80.00"
 console.log(`Final Price:$${calculateDiscount(250, 0.15).toFixed(2)}`); // Expected output: "Final Price: $212.50"
+
+//Task 3 Arrow Function
+
+function calculateServiceFee(amount, serviceType){
+    let serviceFee=0;
+    if (serviceType==="Premium") serviceFee=amount*.15;//add fee for premium
+    else if(serviceType==="Standard") serviceFee=amount*.10;//add fee for standard
+    else serviceFee=amount*.05;//add fee for basic
+    console.log(`Service Fee:$${serviceFee.toFixed(2)}`);//log fee
+}//create fnction for service fee
+
+calculateServiceFee(200, "Premium"); // Expected output: "Service Fee: $30.00"
+calculateServiceFee(500, "Standard"); // Expected output: "Service Fee: $50.00"
